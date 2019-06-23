c8bb1171ad830a3e23636d7fe09afc1310ea2690
sssal <1225941299@qq.com>
2019-06-23 13:44:23 +0800

    git test

--- src/First.java
+++ src/First.java
@@ -1,5 +1,10 @@
 public class First {
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args){
         System.out.println("hellp");
+        System.out.println("Git");
     }
 }
